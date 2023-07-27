d1286269d074d2803e4ace5f5a4bbebd406e8ff2
shem <shemomondi746@gmail.com>
2023-07-26 19:26:51 -0700

    bubble sort corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@ void bubble_sort(int *array, size_t size)
     size_t i, j;
     int temp;
 
-    for (i = 0; i < size - 1; i++)
+    for (i = 0; i < size; i++)
     {
         for (j = 0; j < size - i - 1; j++)
         {
