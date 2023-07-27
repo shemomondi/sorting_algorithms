9334fb38ce468694e15ba004f497ed509d56744d
shem <shemomondi746@gmail.com>
2023-07-26 19:52:08 -0700

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,35 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_H */
+
 #include <stdio.h>
 #include "sort.h"
+
+/* Function prototypes */
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
 
 /**
  * quick_sort - Sorts an array of integers in ascending order using Quick sort
@@ -80,3 +110,4 @@ int partition(int *array, int low, int high, size_t size)
 
 	return (i + 1);
 }
+
